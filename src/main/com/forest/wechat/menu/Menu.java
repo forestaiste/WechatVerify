4c8c548f36b93b71f002e08ea15b9f374ce4ba1d
forestaiste <forestaiste@hotmail.com>
2016-11-21 17:43:50 +0800

    add menu

---
+++ src/main/com/forest/wechat/menu/Menu.java
@@ -0,0 +1,13 @@
+package com.forest.wechat.menu;
+
+public class Menu {
+    private Button[] buttons;
+
+    public Button[] getButtons() {
+        return buttons;
+    }
+
+    public void setButtons(Button[] buttons) {
+        this.buttons = buttons;
+    }
+}
